4da1094223d1e3986855ea4cc8eb404d28c7acd8
Mohammed <almesbahi770027198@gmail.com>
2024-10-16 21:42:23 -1200

    add logger

--- Api/Controllers/v1/EmployeeController.cs
+++ Api/Controllers/v1/EmployeeController.cs
@@ -12,46 +12,96 @@ namespace Api.Controllers.v1
     public class EmployeeController : BaseController
     {
         private readonly IEmployeeService _iEmployeeService;
-        public EmployeeController(IEmployeeService iEmployeeService)
+        private readonly ILogger<EmployeeController> _logger;
+        public EmployeeController(IEmployeeService iEmployeeService, ILogger<EmployeeController> logger)
         {
-            _iEmployeeService= iEmployeeService;
+            _iEmployeeService = iEmployeeService;
+            _logger = logger;
         }
         [HttpPost]
         public async Task<IActionResult> Insert(EmployeeDto dto)
         {
-            var res = await _iEmployeeService.InsertAsync(dto);
-            return Ok(res);
+            try
+            {
+                var res = await _iEmployeeService.InsertAsync(dto);
+                return Ok(res);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
         }
 
         [HttpGet]
         public async Task<IActionResult> Get()
         {
-            var res = await _iEmployeeService.GetAsync();
-            return Ok(res);
+            try
+            {
+                var res = await _iEmployeeService.GetAsync();
+                return Ok(res);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
         }
         [HttpGet("{id}")]
         public async Task<IActionResult> GetById(long id)
         {
-            var res = await _iEmployeeService.GetByIdAsync(id);
-             return Ok(res);
+            try
+            {
+                var res = await _iEmployeeService.GetByIdAsync(id);
+                return Ok(res);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
         }
         [HttpPut("{id}")]
-        public async Task<IActionResult> Update(long id,EmployeeDto dto)
+        public async Task<IActionResult> Update(long id, EmployeeDto dto)
         {
-            var res = await _iEmployeeService.Update(id,dto);
-            return Ok(res);
-        } 
+            try
+            {
+                var res = await _iEmployeeService.Update(id, dto);
+                return Ok(res);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
         [HttpPut("UpdateState")]
         public async Task<IActionResult> UpdateState(long id, State state)
         {
-            var res = await _iEmployeeService.UpdateState(id, state);
-            return Ok(res);
+            try
+            {
+                var res = await _iEmployeeService.UpdateState(id, state);
+                return Ok(res);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
         }
         [HttpDelete("{id}")]
         public async Task<IActionResult> Delete(long id)
         {
-            var res = await _iEmployeeService.Delete(id);
-            return Ok(res);
+            try
+            {
+                var res = await _iEmployeeService.Delete(id);
+                return Ok(res);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
         }
     }
 }
--- Api/Controllers/v1/JobTypeController.cs
+++ Api/Controllers/v1/JobTypeController.cs
@@ -9,46 +9,96 @@ namespace Api.Controllers.v1
     public class JobTypeController : BaseController
     {
         private readonly IJobTypeService _iJobTypeService;
-        public JobTypeController(IJobTypeService jobTypeService)
+        private readonly ILogger<JobTypeController> _logger;
+        public JobTypeController(IJobTypeService jobTypeService, ILogger<JobTypeController> logger)
         {
             _iJobTypeService = jobTypeService;
+            _logger = logger;
         }
         [HttpPost]
         public async Task<IActionResult> Insert(JobTypeDto dto)
         {
-            var res = await _iJobTypeService.InsertAsync(dto);
-            return Ok(res);
+            try
+            {
+                var res = await _iJobTypeService.InsertAsync(dto);
+                return Ok(res);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
         }
 
         [HttpGet]
         public async Task<IActionResult> Get()
         {
-            var res = await _iJobTypeService.GetAsync();
-            return Ok(res);
+            try
+            {
+                var res = await _iJobTypeService.GetAsync();
+                return Ok(res);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
         }
         [HttpGet("{id}")]
         public async Task<IActionResult> GetById(long id)
         {
-            var res = await _iJobTypeService.GetByIdAsync(id);
-            return Ok(res);
+            try
+            {
+                var res = await _iJobTypeService.GetByIdAsync(id);
+                return Ok(res);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
         }
         [HttpPut("{id}")]
         public async Task<IActionResult> Update(long id, JobTypeDto dto)
         {
-            var res = await _iJobTypeService.Update(id, dto);
-            return Ok(res);
+            try
+            {
+                var res = await _iJobTypeService.Update(id, dto);
+                return Ok(res);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
         }
         [HttpPut("UpdateState")]
         public async Task<IActionResult> UpdateState(long id, State state)
         {
-            var res = await _iJobTypeService.UpdateState(id, state);
-            return Ok(res);
+            try
+            {
+                var res = await _iJobTypeService.UpdateState(id, state);
+                return Ok(res);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
         }
         [HttpDelete("{id}")]
         public async Task<IActionResult> Delete(int id)
         {
-            var res = await _iJobTypeService.Delete(id);
-            return Ok(res);
+            try
+            {
+                var res = await _iJobTypeService.Delete(id);
+                return Ok(res);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
         }
     }
 }
